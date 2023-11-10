0ca6250ecd3f93d0b817d99491862f15cb2181e5
chris anatalio <anataliocs@gmail.com>
2023-11-09 18:58:01 -0800

    Create player UI flow

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,7 +3,7 @@ import { useMUD } from "./MUDContext";
 import { singletonEntity, encodeEntity } from "@latticexyz/store-sync/recs";
 import { HistoryTable } from "./HistoryTable";
 import { useEntityQuery } from "@latticexyz/react";
-import { Has, HasValue, getComponentValueStrict } from "@latticexyz/recs";
+import { Has, HasValue, getComponentValue, getComponentValueStrict } from "@latticexyz/recs";
 
 import './index.css'
 import { Navbar } from "./Navbar";
@@ -16,6 +16,7 @@ export const App = () => {
   } = useMUD();
 
   const counter = useComponentValue(Counter, singletonEntity);
+  const playerCreated = getComponentValue(PlayerComponent, playerEntity);
 
   const entities = useEntityQuery([Has(PlayerComponent), Has(PlayerInventoryComponent)]);
 
@@ -29,8 +30,6 @@ export const App = () => {
       : null;
 
   const playerResources = player && player.resourceArray ? player?.resourceArray : [];
-
-  console.log(playerResources);
 
   const resources = playerResources
     .filter((resource) => resource != 0x0000000000000000000000000000000000000000000000000000000000000000)
@@ -48,107 +47,116 @@ export const App = () => {
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
         </div>
+
       </header>
 
-      <main>
+      {!playerCreated ? (
 
-        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-
-          <div className="p-4 gap-2">
-            <div className="grid grid-cols-4 grid-rows-1 gap-2">
-              <div p-4 items-center justify-center>
-                <button
-                  type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
-                  onClick={async (event) => {
-                    event.preventDefault();
-
-                    let createdResource = await initPlayer();
-                    console.log("Player:", createdResource);
-
-                  }}
-                >
-                  Create Player
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            Counter: <span>{counter?.value ?? "??"}</span>
-          </div>
-
-          <div className="p-4 grid grid-cols-4 grid-rows-1 gap-1">
+        <div className="p-4 gap-2">
+          <div className="grid grid-cols-4 grid-rows-1 gap-2">
             <div p-4 items-center justify-center>
               <button
                 type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
                 onClick={async (event) => {
                   event.preventDefault();
-                  let updatedCounter = await increment();
-                  console.log("new counter value:", updatedCounter?.value);
+
+                  let createdResource = await initPlayer();
+                  console.log("Player:", createdResource);
+
                 }}
               >
-                Increment
+                Create Player
               </button>
-
-              {counter?.value && counter.value > 0 ? (
-                <button
-                  type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
-                  onClick={async (event) => {
-                    event.preventDefault();
-                    if (counter?.value > 0) {
-                      let updatedCounter = await decrement();
-                      console.log("New counter value:", updatedCounter?.value);
-                    }
-                  }}
-                >
-                  Decrement
-                </button>
-              ) : null
-              }
-
             </div>
           </div>
-
-          <div className="p-4 gap-2 border bg-gray-100 border-gray-800 shadow-lg rounded-2xl">
-            <div className="grid grid-cols-4 grid-rows-1 gap-2">
-              <div p-4 items-center justify-center>
-                <button
-                  type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
-                  onClick={async (event) => {
-                    event.preventDefault();
-
-                    let createdResource = await mintResource(10n);
-                    console.log("New counter value:", createdResource);
-
-                  }}
-                >
-                  Create Resource
-                </button>
-              </div>
-              <div p-4 items-center justify-center>
-                <h3 className="text-3xl font-bold tracking-tight text-gray-900">Inventory</h3>
-
-                {resources.map((resource) =>
-                  <div>
-
-                    {resource.resource}: {resource.balance}
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
-
-
-          {counter?.value && counter.value > 0 ? (
-            <div>
-              <HistoryTable counterValue={counter?.value} />
-            </div>
-          ) : null
-          }
-
         </div>
-      </main>
-    </div>
 
+      ) : null}
+
+      {playerCreated ? (
+
+        <main>
+
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+
+
+            <div>
+              Counter: <span>{counter?.value ?? "??"}</span>
+            </div>
+
+            <div className="p-4 grid grid-cols-4 grid-rows-1 gap-1">
+              <div p-4 items-center justify-center>
+                <button
+                  type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+                  onClick={async (event) => {
+                    event.preventDefault();
+                    let updatedCounter = await increment();
+                    console.log("new counter value:", updatedCounter?.value);
+                  }}
+                >
+                  Increment
+                </button>
+
+                {counter?.value && counter.value > 0 ? (
+                  <button
+                    type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+                    onClick={async (event) => {
+                      event.preventDefault();
+                      if (counter?.value > 0) {
+                        let updatedCounter = await decrement();
+                        console.log("New counter value:", updatedCounter?.value);
+                      }
+                    }}
+                  >
+                    Decrement
+                  </button>
+                ) : null
+                }
+
+              </div>
+            </div>
+
+            <div className="p-4 gap-2 border bg-gray-100 border-gray-800 shadow-lg rounded-2xl">
+              <div className="grid grid-cols-4 grid-rows-1 gap-2">
+                <div p-4 items-center justify-center>
+                  <button
+                    type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+                    onClick={async (event) => {
+                      event.preventDefault();
+
+                      let createdResource = await mintResource(10n);
+                      console.log("New counter value:", createdResource);
+
+                    }}
+                  >
+                    Create Resource
+                  </button>
+                </div>
+                <div p-4 items-center justify-center>
+                  <h3 className="text-3xl font-bold tracking-tight text-gray-900">Inventory</h3>
+
+                  {resources.map((resource) =>
+                    <div>
+
+                      {resource.resource}: {resource.balance}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+
+
+            {counter?.value && counter.value > 0 ? (
+              <div>
+                <HistoryTable counterValue={counter?.value} />
+              </div>
+            ) : null
+            }
+
+          </div>
+        </main>
+
+      ) : null}
+    </div>
   );
 };
